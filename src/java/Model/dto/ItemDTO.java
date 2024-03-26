103a5130faba3b37a7139db332a4fc0c61830e9c
Rasht 1402 <mahsa.sdanesh@gmail.com>
2024-03-26 16:52:55 -0400

    Added ItemDTO

---
+++ src/java/Model/dto/ItemDTO.java
@@ -0,0 +1,60 @@
+package Model.dto;
+
+public class ItemDTO {
+    private int itemId;
+    private String itemName;
+    private String itemDescription;
+    private String itemQuantity;
+
+    private String date;
+
+    private float price;
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public String getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(String itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
