af2512a4248f91d84481583cc1a8b641fed903bb
tray0019 <tray0019@algonquinlive.com>
2024-04-09 16:24:09 -0400

    Merge origin/master into tomNewbranch
    # Conflicts:
    #	src/java/Controller/LoginServlet.java
    #	src/java/DataAccessLayer/CharityDAO.java
    #	web/Views/charityItems.jsp
    #	web/Views/consumerItems.jsp

---
+++ src/java/Model/dto/ItemDTO.java
@@ -0,0 +1,49 @@
+package Model.dto;
+
+public class ItemDTO {
+    private int itemId;
+    private String itemName;
+    private int itemQuantity;
+    private String date;
+    private float price;
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
---
+++ test/test/LoginTest.java
@@ -0,0 +1,43 @@
+//package test;
+//
+//import DataAccessLayer.UserDAO;
+//import Model.CredentialsDTO;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.JUnitCore;
+//import org.junit.runner.Result;
+//import org.junit.runner.RunWith;
+//import org.junit.runner.notification.Failure;
+//
+////import static org.junit.Assert.*;
+//
+//
+//@RunWith(JUnitPlatform.class)
+//public class LoginTest {
+//
+//    public UserDAO userDAO;
+//
+//    @BeforeEach
+//    public void initial() {
+//        userDAO = new UserDAO();
+//    }
+//
+//    @Test
+//    public void loginTest() {
+//        CredentialsDTO user = userDAO.authenticateUser("mahsa@gmail.com", "123456");
+//        assertNotNull(user);
+//        assertEquals("mahsa@gmail.com", user.getEmailAddress());
+//    }
+//
+//    public static void main(String[] args) {
+//        Result result = JUnitCore.runClasses(LoginTest.class);
+//        for (Failure failure : result.getFailures()) {
+//            System.err.println(failure.toString());
+//        }
+//        if (result.wasSuccessful()) {
+//            System.out.println("All tests passed successfully.");
+//        }
+//    }
+//
+//}
