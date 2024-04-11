74aac92295d8af87202c5e642e50cb5ab60dad53
tray0019 <tray0019@algonquinlive.com>
2024-04-11 13:52:48 -0400

    FInal!
    - Added the the final report
    - I unhighlited/uncommented the test cases. The test cases works on my team member apache net beans but not on my computer so they were highlited. I have problems with libraries.

--- test/test/RegisterTest.java
+++ test/test/RegisterTest.java
@@ -1,87 +1,87 @@
-//package test;
-//
-//import DataAccessLayer.UserDAO;
-//import Model.CharitableOrganizationDTO;
-//import Model.ConsumersDTO;
-//import Model.CredentialsDTO;
-//import Model.RetailersDTO;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.platform.runner.JUnitPlatform;
-//import org.junit.runner.JUnitCore;
-//import org.junit.runner.Result;
-//import org.junit.runner.RunWith;
-//import org.junit.runner.notification.Failure;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@RunWith(JUnitPlatform.class)
-//public class RegisterTest {
-//
-//    public UserDAO userDAO;
-//
-//    @BeforeEach
-//    public void initial() {
-//        userDAO = new UserDAO();
-//    }
-//
-//    @Test
-//    public void registerTest() {
-//        String email = "mahsa@gmail.com";
-//        String password = "123456";
-//        String address = "address";
-//        String phone = "phone";
-//
-//        String userType = "retailer";
-//
-//        // retailer
-//        String retailerName = "retailer_name";
-//
-//        // consumer
-//        String firstName = "Mahsa";
-//        String lastName = "Mahsa";
-//
-//        // charitableOrg
-//        String charityName = "charityName";
-//
-//        UserDAO userDAO = new UserDAO();
-//        CredentialsDTO user = null;
-//
-//        switch (userType) {
-//            case "retailer":
-//                user = new RetailersDTO();
-//                ((RetailersDTO) user).setRetailerName(retailerName);
-//                break;
-//            case "consumer":
-//                user = new ConsumersDTO();
-//                ((ConsumersDTO) user).setFirstName(firstName);
-//                ((ConsumersDTO) user).setLastName(lastName);
-//                break;
-//            case "charitableOrg":
-//                user = new CharitableOrganizationDTO();
-//                ((CharitableOrganizationDTO) user).setCharitableOrgName(charityName);
-//                break;
-//        }
-//
-//        assertNotNull(user);
-//
-//        user.setEmailAddress(email);
-//        user.setPassword(password);
-//        user.setLocation(address);
-//        user.setPhoneNumber(phone);
-//        user.setUserType(userType);
-//
-//        assertTrue(userDAO.addUser(user));
-//    }
-//
-//    public static void main(String[] args) {
-//        Result result = JUnitCore.runClasses(RegisterTest.class);
-//        for (Failure failure : result.getFailures()) {
-//            System.err.println(failure.toString());
-//        }
-//        if (result.wasSuccessful()) {
-//            System.out.println("All tests passed successfully.");
-//        }
-//    }
-//
-//}
+package test;
+
+import DataAccessLayer.UserDAO;
+import Model.CharitableOrganizationDTO;
+import Model.ConsumersDTO;
+import Model.CredentialsDTO;
+import Model.RetailersDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(JUnitPlatform.class)
+public class RegisterTest {
+
+    public UserDAO userDAO;
+
+    @BeforeEach
+    public void initial() {
+        userDAO = new UserDAO();
+    }
+
+    @Test
+    public void registerTest() {
+        String email = "mahsa@gmail.com";
+        String password = "123456";
+        String address = "address";
+        String phone = "phone";
+
+        String userType = "retailer";
+
+        // retailer
+        String retailerName = "retailer_name";
+
+        // consumer
+        String firstName = "Mahsa";
+        String lastName = "Mahsa";
+
+        // charitableOrg
+        String charityName = "charityName";
+
+        UserDAO userDAO = new UserDAO();
+        CredentialsDTO user = null;
+
+        switch (userType) {
+            case "retailer":
+                user = new RetailersDTO();
+                ((RetailersDTO) user).setRetailerName(retailerName);
+                break;
+            case "consumer":
+                user = new ConsumersDTO();
+                ((ConsumersDTO) user).setFirstName(firstName);
+                ((ConsumersDTO) user).setLastName(lastName);
+                break;
+            case "charitableOrg":
+                user = new CharitableOrganizationDTO();
+                ((CharitableOrganizationDTO) user).setCharitableOrgName(charityName);
+                break;
+        }
+
+        assertNotNull(user);
+
+        user.setEmailAddress(email);
+        user.setPassword(password);
+        user.setLocation(address);
+        user.setPhoneNumber(phone);
+        user.setUserType(userType);
+
+        assertTrue(userDAO.addUser(user));
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(RegisterTest.class);
+        for (Failure failure : result.getFailures()) {
+            System.err.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed successfully.");
+        }
+    }
+
+}
--- test/test/RestRegisterTest.java
+++ test/test/RestRegisterTest.java
@@ -1,61 +1,61 @@
-//package test;
-//
-//import org.apache.http.client.methods.HttpPost;
-//import org.apache.http.entity.StringEntity;
-//import org.apache.http.impl.client.CloseableHttpClient;
-//import org.apache.http.impl.client.HttpClients;
-//import org.junit.jupiter.api.Test;
-//import org.junit.platform.runner.JUnitPlatform;
-//import org.junit.runner.JUnitCore;
-//import org.junit.runner.Result;
-//import org.junit.runner.RunWith;
-//import org.junit.runner.notification.Failure;
-//
-//import java.io.IOException;
-//import java.net.http.HttpResponse;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//
-//@RunWith(JUnitPlatform.class)
-//public class RestRegisterTest {
-//
-//    @Test
-//    public void loginTest() {
-//        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-//            String url = "http://localhost:8080/register";
-//            HttpPost httpPost = new HttpPost(url);
-//
-//            String requestBody = """
-//                       {
-//                            "email": "mahsa@gmail.com",
-//                            "password": "123456",
-//                            "phone": "091700000000",
-//                            "address": "address",
-//                            "address": "address",
-//                            "Users": "consumer",
-//                            "first_name": "first_name",
-//                            "last_name": "last_name",
-//                            "phone_num": "phone_num"
-//                       }
-//                    """;
-//            httpPost.setEntity(new StringEntity(requestBody));
-//
-//            HttpResponse<String> execute = (HttpResponse<String>) httpClient.execute(httpPost);
-//            assertEquals(200, execute.statusCode());
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//        Result result = JUnitCore.runClasses(RestRegisterTest.class);
-//        for (Failure failure : result.getFailures()) {
-//            System.err.println(failure.toString());
-//        }
-//        if (result.wasSuccessful()) {
-//            System.out.println("All tests passed successfully.");
-//        }
-//    }
-//
-//}
+package test;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
+
+import java.io.IOException;
+import java.net.http.HttpResponse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@RunWith(JUnitPlatform.class)
+public class RestRegisterTest {
+
+    @Test
+    public void loginTest() {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            String url = "http://localhost:8080/register";
+            HttpPost httpPost = new HttpPost(url);
+
+            String requestBody = """
+                       {
+                            "email": "mahsa@gmail.com",
+                            "password": "123456",
+                            "phone": "091700000000",
+                            "address": "address",
+                            "address": "address",
+                            "Users": "consumer",
+                            "first_name": "first_name",
+                            "last_name": "last_name",
+                            "phone_num": "phone_num"
+                       }
+                    """;
+            httpPost.setEntity(new StringEntity(requestBody));
+
+            HttpResponse<String> execute = (HttpResponse<String>) httpClient.execute(httpPost);
+            assertEquals(200, execute.statusCode());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(RestRegisterTest.class);
+        for (Failure failure : result.getFailures()) {
+            System.err.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed successfully.");
+        }
+    }
+
+}
